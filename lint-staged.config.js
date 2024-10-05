20ed285cf108c0b118fb6fe691a99e0314f65749
romanbuchuk <roman.buchuk@uzhnu.edu.ua>
2024-10-05 10:57:18 +0300

    testing lint-staged 2

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 const config = {
     '*.{js,jsx}': ['eslint --ignore-path .gitignore --fix --ext .js,.jsx ./src'],
-    '.{js,jsx,css}': ['prettier --write'],
+    '*.{js,jsx,css}': ['prettier --write'],
 };
 
 module.exports = config;
--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 // import * as styles from './App.css'; case1
 import styles from './App.css';
-
-const App = () => {return <div className={styles.main}> My First React App </div>;};
+const App = () => {
+    return <div className={styles.main}> My First React App </div>;
+};
 
 export default App;
 
