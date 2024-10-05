58febc7119f2234980b98de171053789c348a3e1
romanbuchuk <roman.buchuk@uzhnu.edu.ua>
2024-10-05 10:04:20 +0300

    Eslint raw configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+// https://eslint.org/docs/latest/use/command-line-interface
+
 const config = {
     env: {
         browser: true,
@@ -6,19 +8,34 @@ const config = {
     },
     extends: [
         'eslint:recommended',
+        /* https://www.npmjs.com/package/eslint-plugin-react */
         'plugin:react/recommended',
+        /* https://www.npmjs.com/package/eslint-plugin-react-hooks */
         'plugin:react-hooks/recommended',
-        'plugin:import/recommended',
+        /*https://www.npmjs.com/package/eslint-plugin-import*/
+        'plugin:import/errors',
+        'plugin:import/warnings',
+        /*https://www.npmjs.com/package/eslint-plugin-unicorn*/
         'plugin:unicorn/recommended',
+        /*https://www.npmjs.com/package/eslint-plugin-cypress*/
+        /*'plugin:cypress/recommended',*/
     ],
+    /* https://github.com/import-js/eslint-plugin-import */
     settings: {
+        react: {
+            version: 'detect', // React version. "detect" automatically picks the version you have installed.
+        },
         'import/resolver': {
             node: {
                 extensions: ['.js', '.jsx'],
             },
+            webpack: {
+                // if not set, import won't work properly with aliases f.e. import Header from '@components/Header'
+                config: './config/webpack.dev.config.js',
+            },
         },
     },
-    plugins: ['react'],
+    plugins: ['simple-import-sort', 'react'],
     ignorePatterns: ['node_modules', '.eslintrc.js'],
 };
 
